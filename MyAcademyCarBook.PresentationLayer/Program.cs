800363486765af602e6071e9cb3a6c6d1f34938f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-10-10 23:10:07 +0300

    Dependency Injection

--- MyAcademyCarBook.PresentationLayer/Program.cs
+++ MyAcademyCarBook.PresentationLayer/Program.cs
@@ -1,3 +1,9 @@
+using MyAcademyCarBook.BusinessLayer.Abstract;
+using MyAcademyCarBook.BusinessLayer.Concrete;
+using MyAcademyCarBook.DataAccessLayer.Abstract;
+using MyAcademyCarBook.DataAccessLayer.Concrete;
+using MyAcademyCarBook.DataAccessLayer.EntityFramework;
+
 namespace MyAcademyCarBook.PresentationLayer
 {
     public class Program
@@ -7,6 +13,9 @@ namespace MyAcademyCarBook.PresentationLayer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<CarBookContext>();
+            builder.Services.AddScoped<IBrandDal,EfBrandDal>();
+            builder.Services.AddScoped<IBrandService,BrandManager>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
